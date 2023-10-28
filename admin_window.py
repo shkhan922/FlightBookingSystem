da2a355bc4d58d62a13b38b0cf45d0569f8ee499
shkhan922 <shkhan922@gmail.com>
2023-10-28 18:29:53 +0530

    added some style

--- admin_window.py
+++ admin_window.py
@@ -1,29 +1,46 @@
 import tkinter as tk
+from tkinter import ttk
 
-class AdminWindow(tk.Frame):
+class AdminWindow(ttk.Frame):
     def __init__(self, master):
         super().__init__(master)
         self.master = master
         self.master.title("Prog2 Travel Agency")
-        self.master.geometry("600x400")
-        self.pack()  # Add this line to pack the AdminWindow
+        self.master.geometry("800x600")
+        self.pack()
 
-        # Image Display
-        self.image_label = tk.Label(self, text="Your image goes here")
-        self.image_label.pack()
+        # Style configuration
+        style = ttk.Style()
+        style.configure("TButton", foreground="black", background="lightblue")
 
-        # Explore Flights Button
-        self.explore_flights_button = tk.Button(self, text="Explore Flights")
-        self.explore_flights_button.pack()
+        # Load and display an image on the admin screen
+        image = tk.PhotoImage(file="agency.png")  # Replace "agency.png" with the path to your image
+        canvas = tk.Canvas(self, width=image.width(), height=image.height())
+        canvas.create_image(0, 0, anchor=tk.NW, image=image)
+        canvas.image = image  # Store a reference to the image
+        canvas.pack()
 
-        # Explore Destinations Button
-        self.explore_destinations_button = tk.Button(self, text="Explore Destinations")
-        self.explore_destinations_button.pack()
+        # Welcome Label
+        welcome_label = ttk.Label(self, text="Welcome, Admin!", font=("Arial", 16))
+        welcome_label.pack()
 
-        # Book a Trip Button
-        self.book_trip_button = tk.Button(self, text="Book a Trip")
-        self.book_trip_button.pack()
+        # Buttons
+        button_frame = ttk.Frame(self)
+        button_frame.pack(side=tk.BOTTOM)
 
-        # Exit Button
-        self.exit_button = tk.Button(self, text="Exit", command=self.master.destroy)
-        self.exit_button.pack()
+        explore_flights_button = ttk.Button(button_frame, text="Explore Flights", style="TButton")
+        explore_flights_button.pack(side=tk.LEFT, padx=10, pady=10)
+
+        explore_destinations_button = ttk.Button(button_frame, text="Explore Destinations", style="TButton")
+        explore_destinations_button.pack(side=tk.LEFT, padx=10, pady=10)
+
+        book_trip_button = ttk.Button(button_frame, text="Book a Trip", style="TButton")
+        book_trip_button.pack(side=tk.LEFT, padx=10, pady=10)
+
+        exit_button = ttk.Button(button_frame, text="Exit", style="TButton", command=self.master.destroy)
+        exit_button.pack(side=tk.LEFT, padx=10, pady=10)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = AdminWindow(root)
+    root.mainloop()
--- login_window.py
+++ login_window.py
@@ -1,6 +1,6 @@
 import tkinter as tk
+from tkinter import ttk
 from admin_window import AdminWindow
-from tkinter import ttk 
 
 class LoginWindow(tk.Frame):
     def __init__(self, master):
@@ -8,40 +8,45 @@ class LoginWindow(tk.Frame):
         self.master = master
         self.master.title("Login")
         self.master.geometry("400x200")
-        self.pack()  # Add this line to pack the LoginWindow
+        self.pack()
 
-
-        # Configure styles
         self.style = ttk.Style()
         self.style.configure("TButton", foreground="black", background="lightblue")
         self.style.configure("TLabel", foreground="black")
         self.style.configure("TEntry", foreground="black")
 
-        # Username Entry
-        self.username_label = tk.Label(self, text="Username:")
-        self.username_label.pack()
-        self.username_entry = tk.Entry(self)
-        self.username_entry.pack()
+        # Create a frame for labels and text entry
+        login_frame = ttk.Frame(self)
+        login_frame.grid(row=0, column=0, padx=10, pady=10)
 
-        # Password Entry
-        self.password_label = tk.Label(self, text="Password:")
-        self.password_label.pack()
-        self.password_entry = tk.Entry(self, show="*")
-        self.password_entry.pack()
+        # Username Label and Entry
+        username_label = ttk.Label(login_frame, text="Username:", style="TLabel")
+        username_label.grid(row=0, column=0, padx=10, pady=10, sticky="w")
+        self.username_entry = ttk.Entry(login_frame, style="TEntry")
+        self.username_entry.grid(row=0, column=1, padx=10, pady=10, sticky="w")
+
+        # Password Label and Entry
+        password_label = ttk.Label(login_frame, text="Password:", style="TLabel")
+        password_label.grid(row=1, column=0, padx=10, pady=10, sticky="w")
+        self.password_entry = ttk.Entry(login_frame, show="*", style="TEntry")
+        self.password_entry.grid(row=1, column=1, padx=10, pady=10, sticky="w")
 
         # Login Button
-        self.login_button = ttk.Button(self, text="Login", command=self.on_successful_login)
-        self.login_button.pack()
-        
+        login_button = ttk.Button(login_frame, text="Login", style="TButton", command=self.on_successful_login)
+        login_button.grid(row=2, column=0, columnspan=2, padx=10, pady=10, sticky="we")
 
-        # Exit Button
-        self.exit_button = ttk.Button(self, text="Exit", command=self.master.destroy)
-        self.exit_button.pack()
-        
+         # Login Button
+        login_button = ttk.Button(login_frame, text="Exit", style="TButton", command=self.master.destroy)
+        login_button.grid(row=2, column=2, columnspan=2, padx=10, pady=10, sticky="we")
+
     def on_successful_login(self):
         username = self.username_entry.get()
-        # Check username and password here
         if username == "admin":
             admin_app = AdminWindow(self.master)
             self.pack_forget()  # Hide the LoginWindow
             admin_app.pack()  # Display the AdminWindow
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = LoginWindow(root)
+    root.mainloop()
